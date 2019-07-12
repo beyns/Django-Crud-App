6c45a69e44c4f8308e91522313d9b69a4e307b44
vincent <fvince12@gmail.com>
2019-07-12 15:17:20 +0800

    ...
    Signed-off-by: vincent <fvince12@gmail.com>

--- product/views.py
+++ product/views.py
@@ -1,8 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ProductForm
 from .models import Product
 # Create your views here.
+
+
+def create_product(request):
+
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, "product/details.html", context)
 
 
 def product_detail_view(request):
